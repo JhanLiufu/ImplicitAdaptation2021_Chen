20f0a56f936f0bc5356d90cf4927d1e334af80ac
JohnLauFoo <mliufu@uchicago.edu>
2021-11-20 01:05:46 -0600

    Cursor/Endpoint Signed Error

--- Processing/ControllerProcessing_CursorEndpoinContrast.m
+++ Processing/ControllerProcessing_CursorEndpoinContrast.m
@@ -1,6 +1,6 @@
 %% This code is for controller processing, plot Cursor/Endpoint feedback abs errors
-%% To be placed in the DATA file to access both FPSC and OnlineCursor folders
-% Written by Mengzhan Liufu at UChicago. 
+%% To be placed in the \DATA file to access both FPSC and OnlineCursor folders
+% Written by Mengzhan Liufu at UChicago.
 xCenter = 960;
 yCenter = 540;
 finaldata = [0 0];
@@ -12,15 +12,19 @@ FPSC_subject_list_size = size(FPSC_subject_list_num);
 FPSC_subject_list_letter = char(FPSC_subject_list_num+'A'-1);
 FPSC_subject_list_length = FPSC_subject_list_size(2);
 FPSC_allsubject_abserr = zeros(FPSC_subject_list_length,210);
+FPSC_allsubject_signederr = zeros(FPSC_subject_list_length,210);
 FPSC_avgsubject_abserr = zeros(1,210);
+FPSC_avgsubject_signederr = zeros(1,210);
 
 % Calculate All Subjects Avg Abs Err for Cursor Feedback grouop
-Cursor_subject_list_num = [1 2 3];
+Cursor_subject_list_num = [1 2 3 4];
 Cursor_subject_list_size = size(Cursor_subject_list_num);
 Cursor_subject_list_letter = char(Cursor_subject_list_num+'A'-1);
 Cursor_subject_list_length = Cursor_subject_list_size(2);
 Cursor_allsubject_abserr = zeros(Cursor_subject_list_length,210);
+Cursor_allsubject_signederr = zeros(Cursor_subject_list_length,210);
 Cursor_avgsubject_abserr = zeros(1,210);
+Cursor_avgsubject_signederr = zeros(1,210);
 
 %the DATA folder directory
 root_dir = pwd;
@@ -28,7 +32,7 @@ root_dir = pwd;
 % Log FPSC group data, calculate abserr across trials
 cd('FPSC');
 currentdataset = pwd;
-for p = 1:11
+for p = 1:FPSC_subject_list_length
     cd(FPSC_subject_list_letter(p));
     cd("Controller");
     currentfolder = pwd;
@@ -56,7 +60,9 @@ for p = 1:11
             targety = targety - yCenter;
 
             abserr = abs(atand(finaly/finalx)-atand(targety/targetx));
+            signederr = atand(finaly/finalx)-atand(targety/targetx);
             FPSC_allsubject_abserr(p,(i-1)*30+j) = abserr;
+            FPSC_allsubject_signederr(p,(i-1)*30+j) = signederr;
         end
         cd(currentfolder)
     end
@@ -85,7 +91,9 @@ for p = 1:11
             targety = yCenter - targety;
 
             abserr = abs(atand(finaly/finalx)-atand(targety/targetx));
+            signederr = atand(finaly/finalx)-atand(targety/targetx);
             FPSC_allsubject_abserr(p,(i-1)*30+j) = abserr;
+            FPSC_allsubject_signederr(p,(i-1)*30+j) = signederr;
         end
         cd(currentfolder);
     end
@@ -94,6 +102,7 @@ end
 
 for m = 1:210 
     FPSC_avgsubject_abserr(1,m) = sum(FPSC_allsubject_abserr(:,m))/FPSC_subject_list_length;
+    FPSC_avgsubject_signederr(1,m) = sum(FPSC_allsubject_signederr(:,m))/FPSC_subject_list_length;
 end
 
 cd(root_dir);
@@ -101,7 +110,7 @@ cd('Controller_OnlineCursor');
 
 % Log Cursor group data, calculate abserr across trials
 currentdataset = pwd;
-for p = 1:3
+for p = 1:Cursor_subject_list_length
     cd(Cursor_subject_list_letter(p));
     %cd("Controller");
     currentfolder = pwd;
@@ -129,7 +138,9 @@ for p = 1:3
             targety = targety - yCenter;
 
             abserr = abs(atand(finaly/finalx)-atand(targety/targetx));
+            signederr = atand(finaly/finalx)-atand(targety/targetx);
             Cursor_allsubject_abserr(p,(i-1)*30+j) = abserr;
+            Cursor_allsubject_signederr(p,(i-1)*30+j) = signederr;
         end
         cd(currentfolder)
     end
@@ -158,7 +169,9 @@ for p = 1:3
             targety = yCenter - targety;
 
             abserr = abs(atand(finaly/finalx)-atand(targety/targetx));
+            signederr = atand(finaly/finalx)-atand(targety/targetx);
             Cursor_allsubject_abserr(p,(i-1)*30+j) = abserr;
+            Cursor_allsubject_signederr(p,(i-1)*30+j) = signederr;
         end
         cd(currentfolder);
     end
@@ -169,23 +182,52 @@ cd(root_dir);
 
 for m = 1:210 
     Cursor_avgsubject_abserr(1,m) = sum(Cursor_allsubject_abserr(:,m))/Cursor_subject_list_length;
+    Cursor_avgsubject_signederr(1,m) = sum(Cursor_allsubject_signederr(:,m))/Cursor_subject_list_length;
 end
 
 % Plot absolute error
 figure;
 hold on;
 set(gca, 'linewidth', 1.1, 'fontsize', 16, 'fontname', 'times');
-xlabel('Trial Number')
+xlabel('Trial #')
 ylabel('Absolute Error (Degree)')
 title('Absolute Reach Error on each Trial')
-plot(FPSC_avgsubject_abserr,'ok-','linewidth',1.1,'markerfacecolor',[254, 67, 101]/255);
-plot(Cursor_avgsubject_abserr,'ok-','linewidth',1.1,'markerfacecolor',[38, 188, 213]/255);
-% ar = area(0:25,61:90);
-% ar.FaceColor = [200,200,169]/255;
-% ar.FaceAlpha = 0.5;
-legend('Endpoint','Cursor')
-grid on;
+
+FPSC_plot_abs = plot(FPSC_avgsubject_abserr,'ok-','color','r','linewidth',1.1,'markerfacecolor','r');
+set(FPSC_plot_abs,{'DisplayName'},{'Endpoint'})
+
+Cursor_plot_abs = plot(Cursor_avgsubject_abserr,'ok-','color','b','linewidth',1.1,'markerfacecolor','b');
+set(Cursor_plot_abs,{'DisplayName'},{'Cursor'})
+
+Endpoint_x_NVF = [60,60,90,90];
+Endpoint_y_NVF = [0,25,25,0];
+h1 = fill(Endpoint_x_NVF,Endpoint_y_NVF,[128, 128, 128]/255);
+set(h1,'edgealpha',0,'facealpha',0.3,{'DisplayName'},{'No Visual'});
 axis([0 210 0 25])
+legend
+grid on;
+
+% Plot signed error
+figure;
+hold on;
+set(gca, 'linewidth', 1.1, 'fontsize', 16, 'fontname', 'times');
+xlabel('Trial #')
+ylabel('Reach Direction Relative to target (Degree)')
+title('Signed Reach Error on each Trial')
+
+FPSC_plot_signed = plot(FPSC_avgsubject_signederr,'ok-','color','r','linewidth',1.1,'markerfacecolor','r');
+set(FPSC_plot_signed,{'DisplayName'},{'Endpoint'})
+
+Cursor_plot_signed = plot(Cursor_avgsubject_signederr,'ok-','color','b','linewidth',1.1,'markerfacecolor','b');
+set(Cursor_plot_signed,{'DisplayName'},{'Cursor'})
+
+Cursor_x_NVF = [60,60,90,90];
+Cursor_y_NVF = [-15,15,15,-15];
+h2 = fill(Cursor_x_NVF,Cursor_y_NVF,[128, 128, 128]/255);
+set(h2,'edgealpha',0,'facealpha',0.3,{'DisplayName'},{'No Visual'});
+axis([0 210 -15 15])
+legend
+grid on;
 
 % ANOVA analysis for Cursor reversal blocks and Endpoint reversal blocks
 Reversal_avgsubject_abserr = zeros(1,180);
