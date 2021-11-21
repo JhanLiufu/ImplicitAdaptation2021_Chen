33201ae0f2ff27145afb57908945d8a75cf55012
JohnLauFoo <mliufu@uchicago.edu>
2021-11-21 02:43:28 -0600

    Avg abs err by block bar plot

--- Processing/ControllerProcessing_CursorEndpoinContrast.m
+++ Processing/ControllerProcessing_CursorEndpoinContrast.m
@@ -15,6 +15,8 @@ FPSC_allsubject_abserr = zeros(FPSC_subject_list_length,210);
 FPSC_allsubject_signederr = zeros(FPSC_subject_list_length,210);
 FPSC_avgsubject_abserr = zeros(1,210);
 FPSC_avgsubject_signederr = zeros(1,210);
+FPSC_avgsubject_block_abserr = zeros(1,7);
+FPSC_avg_block_error = zeros(1,7);
 
 % Calculate All Subjects Avg Abs Err for Cursor Feedback grouop
 Cursor_subject_list_num = [1 2 3 4];
@@ -25,6 +27,8 @@ Cursor_allsubject_abserr = zeros(Cursor_subject_list_length,210);
 Cursor_allsubject_signederr = zeros(Cursor_subject_list_length,210);
 Cursor_avgsubject_abserr = zeros(1,210);
 Cursor_avgsubject_signederr = zeros(1,210);
+Cursor_avgsubject_block_abserr = zeros(1,7);
+Cursor_avg_block_error = zeros(1,7);
 
 %the DATA folder directory
 root_dir = pwd;
@@ -185,6 +189,54 @@ for m = 1:210
     Cursor_avgsubject_signederr(1,m) = sum(Cursor_allsubject_signederr(:,m))/Cursor_subject_list_length;
 end
 
+for k = 1:30
+    FPSC_avgsubject_block_abserr(1,1) = FPSC_avgsubject_block_abserr(1,1) + FPSC_avgsubject_abserr(1,k);
+    Cursor_avgsubject_block_abserr(1,1) = Cursor_avgsubject_block_abserr(1,1) + Cursor_avgsubject_abserr(1,k);
+end
+
+for k = 31:60
+    FPSC_avgsubject_block_abserr(1,2) = FPSC_avgsubject_block_abserr(1,2) + FPSC_avgsubject_abserr(1,k);
+    Cursor_avgsubject_block_abserr(1,2) = Cursor_avgsubject_block_abserr(1,2) + Cursor_avgsubject_abserr(1,k);
+end
+
+for k = 61:90
+    FPSC_avgsubject_block_abserr(1,3) = FPSC_avgsubject_block_abserr(1,3) + FPSC_avgsubject_abserr(1,k);
+    Cursor_avgsubject_block_abserr(1,3) = Cursor_avgsubject_block_abserr(1,3) + Cursor_avgsubject_abserr(1,k);
+end
+
+for k = 91:120
+    FPSC_avgsubject_block_abserr(1,4) = FPSC_avgsubject_block_abserr(1,4) + FPSC_avgsubject_abserr(1,k);
+    Cursor_avgsubject_block_abserr(1,4) = Cursor_avgsubject_block_abserr(1,4) + Cursor_avgsubject_abserr(1,k);
+end
+
+for k = 121:150
+    FPSC_avgsubject_block_abserr(1,5) = FPSC_avgsubject_block_abserr(1,5) + FPSC_avgsubject_abserr(1,k);
+    Cursor_avgsubject_block_abserr(1,5) = Cursor_avgsubject_block_abserr(1,5) + Cursor_avgsubject_abserr(1,k);
+end
+
+for k = 151:180
+    FPSC_avgsubject_block_abserr(1,6) = FPSC_avgsubject_block_abserr(1,6) + FPSC_avgsubject_abserr(1,k);
+    Cursor_avgsubject_block_abserr(1,6) = Cursor_avgsubject_block_abserr(1,6) + Cursor_avgsubject_abserr(1,k);
+end
+
+for k = 181:210
+    FPSC_avgsubject_block_abserr(1,7) = FPSC_avgsubject_block_abserr(1,7) + FPSC_avgsubject_abserr(1,k);
+    Cursor_avgsubject_block_abserr(1,7) = Cursor_avgsubject_block_abserr(1,7) + Cursor_avgsubject_abserr(1,k);
+end
+
+for i = 1:7
+    FPSC_avgsubject_block_abserr(1,i) = FPSC_avgsubject_block_abserr(1,i)/30;
+    Cursor_avgsubject_block_abserr(1,i) = Cursor_avgsubject_block_abserr(1,i)/30;
+    FPSC_block_error = 0;
+    Cursor_block_error = 0;
+    for j = (i-1)*30+1 : i*30
+        FPSC_block_error = FPSC_block_error + abs(FPSC_avgsubject_block_abserr(1,i)-FPSC_avgsubject_abserr(1,j));
+        Cursor_block_error = Cursor_block_error + abs(Cursor_avgsubject_block_abserr(1,i)-Cursor_avgsubject_abserr(1,j));
+    end
+    FPSC_avg_block_error(i) = FPSC_block_error/30;
+    Cursor_avg_block_error(i) = Cursor_block_error/30;
+end
+
 % Plot absolute error
 figure;
 hold on;
@@ -229,20 +281,41 @@ axis([0 210 -15 15])
 legend
 grid on;
 
+% Plot FPSC group abs err by block average
+blocks = 1:7;
+figure;
+set(gca, 'linewidth', 1.1, 'fontsize', 16, 'fontname', 'times');
+hold on;
+sgtitle('Average Absolute Error By Block')
+subplot(1,2,1);
+hold on;
+title('Endpoint Feedback Group')
+bar(blocks,FPSC_avgsubject_block_abserr);
+er1 = errorbar(blocks,FPSC_avgsubject_block_abserr,FPSC_avg_block_error);
+ylabel('Average Absolute Error (Degree)')
+xlabel('Block #')
+
+subplot(1,2,2);
+hold on;
+title('Cursor Feedback Group')
+bar(blocks,Cursor_avgsubject_block_abserr);
+er2 = errorbar(blocks,Cursor_avgsubject_block_abserr,Cursor_avg_block_error);
+xlabel('Block #')
+
 % ANOVA analysis for Cursor reversal blocks and Endpoint reversal blocks
-Reversal_avgsubject_abserr = zeros(1,180);
-for k = 91:210
-    Reversal_avgsubject_abserr(k-90) = FPSC_avgsubject_abserr(k);
-    Reversal_avgsubject_abserr(k+30) = Cursor_avgsubject_abserr(k);
-end
-
-Reversal_group = cell(1,240);
-for m = 1:120
-    Reversal_group{1,m} = 'Endpoint Feedback Reversal';
-end
-
-for m = 121:240
-    Reversal_group{1,m} = 'Cursor Feedback Reversal';
-end
-
-[p,tbl,stats] = anova1(Reversal_avgsubject_abserr,Reversal_group);
+% Reversal_avgsubject_abserr = zeros(1,180);
+% for k = 91:210
+%     Reversal_avgsubject_abserr(k-90) = FPSC_avgsubject_abserr(k);
+%     Reversal_avgsubject_abserr(k+30) = Cursor_avgsubject_abserr(k);
+% end
+% 
+% Reversal_group = cell(1,240);
+% for m = 1:120
+%     Reversal_group{1,m} = 'Endpoint Feedback Reversal';
+% end
+% 
+% for m = 121:240
+%     Reversal_group{1,m} = 'Cursor Feedback Reversal';
+% end
+% 
+% [p,tbl,stats] = anova1(Reversal_avgsubject_abserr,Reversal_group);
